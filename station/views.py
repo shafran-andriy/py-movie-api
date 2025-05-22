147a353493696602cf1baedd6d793310eaf4a115
shafran <shafran.andriy@gmail.com>
2025-05-22 23:47:16 +0300

    Created project and app

--- station/views.py
+++ station/views.py
@@ -19,8 +19,10 @@ def bus_list(request):
         serializer = BusSerializer(data=request.data, many=False)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
