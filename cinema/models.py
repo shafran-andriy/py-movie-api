6818bd529f3984c06f44a81b5d797097880a417e
shafran <shafran.andriy@gmail.com>
2025-05-22 23:55:43 +0300

    Created progect and app

---
+++ cinema/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return self.title
